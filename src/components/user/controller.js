4cc50570fdcff828890b8cf085354e7dc2e6c431
Anastasija3587 <Anastasija3587gmail.com>
2020-02-19 19:49:03 +0200

    fixed controller user

--- src/components/user/controller.js
+++ src/components/user/controller.js
@@ -1,25 +1,21 @@
 // loginUser, registerUser
 const User = require('./model');
 
-const registerUser = async (req, res) => {
-  try {
-    const result = await User.createUser(req.body);
-    res.status(201).json(result);
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-};
-
-const loginUser = async (req, res) => {
-  try {
-    const result = await User.createUser(req.body);
-    res.status(201).json(result);
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-};
-
 module.exports = {
-  registerUser,
-  loginUser,
+  async registerUser(req, res) {
+    try {
+      const result = await User.createUser(req.body);
+      res.status(201).json(result);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  },
+  async loginUser(req, res) {
+    try {
+      const result = await User.createUser(req.body);
+      res.status(201).json(result);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  },
 };
