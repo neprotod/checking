caf449458ed245d4d95adafbbf1d47a89955876a
Roman Pryymak <prima3110@ukr.net>
2020-02-23 19:06:20 +0200

    priorities

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 const config = require('../../config');
 module.exports = async () => {
-  try{
-      await mongoose.connect(process.env.DB_CONNECTION, {
-          useNewUrlParser: true,
-          useCreateIndex: true,
-          useUnifiedTopology: true,
-          useFindAndModify: false
-      });
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
 
-      console.log('MongoDB is connection');
-  }catch(e){
-      console.error(e);
+    console.log('MongoDB is connection');
+  } catch (e) {
+    console.error(e);
   }
-}
+};
