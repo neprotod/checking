27de834ce3cd59fa45b579863d187f53966237af
Alexander Podlesnyi <gwebdzen@gmail.com>
2020-03-06 23:23:50 +0200

    Replace router

--- src/components/task/routing.js
+++ src/components/task/routing.js
@@ -7,10 +7,12 @@ const taskController = require('./controller');
 
 route.get('/', taskController.getAllUserTask);
 route.post('/', validate.createTask, taskController.createTask);
+route.get('/priority', taskController.getAllPriority);
 route.put('/:id', validate.updateTask, taskController.updateTask);
 route.get('/:id', taskController.getTask);
 route.delete('/:id', taskController.deleteTask);
 
-route.get('/priority', taskController.getAllPriority);
+
+
 
 module.exports = route;
