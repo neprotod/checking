3184d1b936f4fe61c8748d33ac2269103e451b36
Ivan <lysko1999i@gmail.com>
2020-03-11 15:25:53 +0200

    added month and year date filtres for statistic and fix burned tasks

--- src/utils/MatchAdapter.js
+++ src/utils/MatchAdapter.js
@@ -79,13 +79,37 @@ class MatchAdapter {
   }
 
   /**
+   *Get first date in the year to current date
+   *
+   * @return {{}} return first date in the year to current date  filter
+   */
+  actionYear() {
+    const startYearDate = changedDate.getStartYear();
+    const currentDate = changedDate.getDate();
+
+    return {start_date: {$gt: startYearDate, $lte: currentDate}};
+  }
+
+  /**
+   *Get first date in the month to current date
+   *
+   * @return {{}} return first date in the month to current date  filter
+   */
+  actionMonth() {
+    const startMonthDate = changedDate.getStartMonth();
+    const currentDate = changedDate.getDate();
+
+    return {start_date: {$gt: startMonthDate, $lte: currentDate}};
+  }
+
+  /**
    *Get burned filter
    *
    * @return {{}} return burned filter
    */
   actionBurned() {
     const burnedDate = changedDate.getDate();
-    return {end_date: {$lte: burnedDate}};
+    return {end_date: {$lte: burnedDate}, done: false};
   }
 
   /**
