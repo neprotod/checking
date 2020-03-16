b758e1cbe2ca450330a0c234642a04a550bbc239
Alexander Podlesnyi <gwebdzen@gmail.com>
2020-03-16 23:27:04 +0200

    Hardcore url to goole

--- config.js
+++ config.js
@@ -3,4 +3,5 @@ module.exports = {
   jwt_secret: 'fs1dfg4dgd56fh4sr3tg',
   expire: null,
   googleCallback: 'http://localhost:3030/api/user/google/callback',
+  to_login: 'http://localhost:3000/login'
 };
