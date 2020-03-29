c8894435f4df6e95db8ad33dcc46ec56c4833df6
Alexander Podlesnyi <gwebdzen@gmail.com>
2020-03-29 20:45:24 +0300

    Build 2

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a6facb6006ed164a661e5366d86694b.js"
+  "/precache-manifest.1fcb2c14fc944938e578989c9bc6d127.js"
 );
 
 self.addEventListener('message', (event) => {
